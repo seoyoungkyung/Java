7ee15627baf07157f41d6ad04ecd2abc5eda4071
seoyoungkyung <dnsdka202@naver.com>
2024-08-13 16:07:37 +0900

    second push

---
+++ Java20240813/src/ex03/자동변환.java
@@ -0,0 +1,19 @@
+package ex03;
+
+/*
+ * 작은 자료형은 큰자료형에 자동으로 형변환되서 저장됨
+ */
+public class 자동변환 {
+		
+	public static void main(String[] args) {
+		
+		int num1 = 10;	//10은 메모리공간에 int형으로 저장됨
+		
+		double d1 = 10;	//
+		
+		long l1 = 100;	//
+		
+		float f1 = l1;	/\
+		
+	}
+}
